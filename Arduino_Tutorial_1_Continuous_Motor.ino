5ba5012be9a8cd3c084c6c2cc28bed6dfa08bdd2
Greg <g.sovaridis@ghoz-software.be>
2017-05-25 22:23:00 +0200

    Create Arduino_Tutorial_1_Continuous_Motor.ino

---
+++ Arduino_Tutorial_1_Continuous_Motor.ino
@@ -0,0 +1,28 @@
+/**
+ * MakiBot Lab 
+ * 
+ * Arduino Tutorial #1: Continuous Motor
+ * 25-05-2017
+ */
+int motorPin = 2;
+
+void setup() 
+{
+  // Open serial port
+  Serial.begin(9600);
+
+  pinMode(motorPin, OUTPUT);
+}
+
+void loop()
+{
+  Serial.println("Moteur On");
+  
+  digitalWrite(motorPin, HIGH); // le moteur se lance
+  delay(2000);
+  
+  Serial.println("Moteur Off");
+
+  digitalWrite(motorPin, LOW); // le moteur s'arrête
+  delay(2000);
+}
